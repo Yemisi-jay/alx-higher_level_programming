a799c7351870a1e6e02f2d51c55e3ce8954f6cfc
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-11-28 08:22:33 -0800

    print possible combination of numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(100):
     if (number != 99):
         print("{}{}, ".format(int(number / 10), number % 10), end="")
     else:
-        print("{}{}".format(int(number /10), number %10))
+        print("{}{}".format(int(number / 10), number % 10))
