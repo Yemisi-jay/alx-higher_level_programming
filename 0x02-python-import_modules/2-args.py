6a927fc68f8f57bd258a55e833f5c32ce0da01d3
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-11-30 09:05:32 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if(args <= 1):
         print("{} argument.".format(args - 1))
     elif(args == 2):
-        print("{} argument:".format(args - 1))
+        print("{} arguments:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
     for x in range(1, args):
