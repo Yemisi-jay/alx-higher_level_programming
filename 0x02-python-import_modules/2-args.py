2e53d0254643f36ff2fe9bf5a83a7b3e96f8392e
Yemisi-jay <anuoluwayemisi@gmail.com>
2023-12-01 04:43:30 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    args = len(sys.argv)
-    if(args <= 1):
-        print("{} arguments.".format(args - 1))
-    elif(args == 2):
-        print("{} arguments:".format(args - 1))
-    else:
-        print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
+if __name__ == '__main__':
+    from sys import argv
+
+if len(argv) < 2:
+    print("0 arguments.")
+elif len(argv) == 2:
+    print(f"1 argument:")
+    print(f"1: {argv[1]}")
+else:
+    print(f"{len(argv) - 1} arguments:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
